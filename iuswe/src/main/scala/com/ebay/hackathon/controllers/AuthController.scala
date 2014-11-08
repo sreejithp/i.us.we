0417ce6757af56aae48168b0802d619f32d12492
Sreejith <sreejith.net@gmail.com>
2014-11-08 20:17:56 +0530

    User Registration and Authentication

--- iuswe/src/main/scala/com/ebay/hackathon/controllers/AuthController.scala
+++ iuswe/src/main/scala/com/ebay/hackathon/controllers/AuthController.scala
@@ -1,10 +1,10 @@
 package com.ebay.hackathon.controllers
 
-import java.io.Serializable
-import java.net.{URLDecoder, URLEncoder}
 import javax.servlet.http.HttpSession
 
-import com.ebay.hackathon.endpoints.{StockResponses, Response, HttpSessionSupport}
+import com.ebay.hackathon.dao.UserDao
+import com.ebay.hackathon.endpoints.{HttpSessionSupport, Response, StockResponses}
+import com.ebay.hackathon.entity.User
 import org.bson.types.ObjectId
 
 
@@ -14,6 +14,20 @@ object AuthController extends HttpSessionSupport {
   def directLogin(email: String, password: String)(implicit session: HttpSession): Response = {
     try {
       LOGGER.debug("Try user login " + email)
+      val user = UserDao.getUserByEmail(email)
+
+      if (user == null) {
+        LOGGER.warn("Login attempted for a user which does not exist: " + email)
+        StockResponses.ERROR_NOT_FOUND
+      } else {
+
+        if (user.password != null && user.password.equals(password)) {
+          //TODO: MD5Hex
+          setLoginSession(user)
+        } else {
+          StockResponses.ERROR_UNAUTHORIZED
+        }
+      }
       StockResponses.OK // TODO;
 
     } catch {
@@ -25,10 +39,25 @@ object AuthController extends HttpSessionSupport {
   def register(email: String,
                password: String,
                name: String,
-               subscriptionId: String)(implicit session: HttpSession): Response = {
+               address: String,
+               userType: Int,
+               pledge: Int,
+               pledgeDay: Int,
+               pledgeWeekDay: Int)(implicit session: HttpSession): Response = {
     try {
-//      setLoginSession(user)
-      StockResponses.OK // TODO
+      val user = new User()
+      user.email = email
+      user.name = name
+      user.address = address
+      user.userType = userType
+      user.pledge = pledge
+      user.pledgeDay = pledgeDay
+      user.pledgeWeekDay = pledgeWeekDay
+      user.password = password
+
+      val registeredUser = UserDao.createUser(user)
+      setLoginSession(registeredUser)
+      Response(registeredUser)
 
     } catch {
       case e: Exception => LOGGER.error("Could not register ", e); Response(e)
